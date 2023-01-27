62977845bc1bb0ec3d9049e26ecdd4742c225e40
lalo <jorgegil9706@gmail.com>
2023-01-27 14:51:06 -0500

    workout

--- pages/workout.js
+++ pages/workout.js
@@ -3,7 +3,13 @@ import { useState } from "react";
 import styles from "./index.module.css";
 
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
+  // weight,age,ft,inch,goal
+  const [weight, setWeight] = useState(220);
+  const [age, setAge] = useState(25);
+  const [ft, setFt] = useState(5);
+  const [inch, setInch] = useState(10);
+  const [goal, setGoal] = useState("lose weight");
+
   const [result, setResult] = useState();
 
   async function onSubmit(event) {
@@ -42,14 +48,57 @@ export default function Home() {
         <img src="/robot_icon.png" className={styles.icon} />
         <h3>Workout Generator</h3>
         <form onSubmit={onSubmit}>
+        <label>Weight</label>
+          <input
+            type="number"
+            min={1}
+            max={99}
+            name="weight"
+            placeholder="Enter the weight"
+            value={weight}
+            onChange={(e) => setWeight(Number.parseInt(e.target.value))}
+          />
+
+          <label>Age</label>
+          <input
+            type="number"
+            min={1}
+            max={99}
+            name="age"
+            placeholder="Enter the age"
+            value={age}
+            onChange={(e) => setAge(Number.parseInt(e.target.value))}
+          />
+
+          <label>Height</label>
+          <input
+            type="number"
+            min={1}
+            name="ft"
+            placeholder="5Ft"
+            value={ft}
+            onChange={(e) => setFt(Number.parseInt(e.target.value))}
+          />
+
+          <label>Height</label>
+          <input
+            type="number"
+            min={1}
+            name="in"
+            placeholder="10in"
+            value={inch}
+            onChange={(e) => setPriceMax(Number.parseInt(e.target.value))}
+          />
+
+          <label>Hobbies</label>
           <input
             type="text"
-            name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
+            name="goal"
+            placeholder="Lose weight or improve strength, balance ...."
+            value={goal}
+            onChange={(e) => setHobbies(e.target.value)}
           />
-          <input type="submit" value="Generate names" />
+          <input type="submit" value="Generate Workout" />
         </form>
         <div className={styles.result}>{result}</div>
       </main>
