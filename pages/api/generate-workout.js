e2b76d01a4397ded1e0c46da93420311e9aa62a4
zach <zachalvarezxvi@gmail.com>
2023-01-28 20:15:43 -0500

    update temperature to 0.6

--- pages/api/generate-workout.js
+++ pages/api/generate-workout.js
@@ -23,7 +23,7 @@ export default async function (req, res) {
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: generatePrompt(weight,age,ft,inch,goal),
-      temperature: 0.7,
+      temperature: 0.6,
       max_tokens: 200,
     });
     res.status(200).json({ result: completion.data.choices[0].text });
